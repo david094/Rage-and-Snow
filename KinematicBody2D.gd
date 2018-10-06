0e59d3a77e0348e490cbfd89b6f764c2fc527b40
daaf <daaf1994@gmail.com>
2018-10-05 21:21:41 -0300

    looking down the cross, speak no evil

---
+++ KinematicBody2D.gd
@@ -0,0 +1,46 @@
+extends KinematicBody2D
+
+export (PackedScene) var bola_escena
+export (NodePath) var bola_spawn
+export (int) var vel_bola
+export (Vector2) var dir
+
+onready var bola_pos = get_node(bola_spawn)
+
+func _ready():
+	pass
+
+
+func _input(event):
+	if event.is_action_released("ui_select"):
+		disparo()
+
+func disparo():
+	dir = Vector2(1,0)
+	var bola = bola_escena.instance()
+	bola.set_global_position(bola_pos.get_global_position())
+	
+	bola.disparo(vel_bola,dir)
+	get_parent().add_child(bola)
+	
+const UP = Vector2(0,-1)
+const SPEED = 200
+const ALTURA = -550
+const GRAVEDAD = 20
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	motion.y += GRAVEDAD
+	if Input.is_action_pressed("ui_right"):
+		motion.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -SPEED
+	else:
+		motion.x = 0
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = ALTURA
+	motion = move_and_slide(motion,UP)
+	pass
---
+++ bola.gd
@@ -0,0 +1,19 @@
+extends RigidBody2D
+export (int) var vida
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func disparo(vel_bola,dir):
+	apply_impulse(Vector2(0,0),dir*vel_bola)
+	
+func _process(delta):
+	vida -= delta
+	if vida <= 0:
+		get_parent().remove_child(self)
+	pass
